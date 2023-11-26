20ee4bd58fef4c97095ab4e628fade6766f1231e
sharad-0-1 <loganman8979@gmail.com>
2023-11-27 02:56:21 +0530

    fff

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
-import Featured from "@/components/featured/Featured";
-import CategoryList from "@/components/categoryList/CategoryList";
-import CardList from "@/components/cardList/CardList";
+import Featured from "../components/featured/Featured";
+import CategoryList from "../components/CategoryList/CategoryList";
+import CardList from "../components/cardList/CardList";
 
 export default function Home({ searchParams }) {
   const page = parseInt(searchParams.page) || 1;
